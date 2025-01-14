0af9635bc786e0d05ec2b3d9aa19a7dd73a14139
acia <acia.chouchane@etu.univ-st-etienne.fr>
2025-01-14 15:41:06 +0100

    Mis en place des pages web

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AccueilController extends AbstractController
+{
+    #[Route('/accueil', name: 'app_accueil')]
+    public function accueil(): Response
+    {
+        return $this->render('accueil/accueil.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('accueil/about.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+
+    #[Route('/cv', name: 'app_cv')]
+    public function cv(): Response
+    {
+        return $this->render('accueil/cv.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }   
+
+    #[Route('/portfolio_1', name: 'app_portfolio_1')]
+    public function portfolio_1(): Response
+    {
+        return $this->render('accueil/portfolio_1.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+
+    #[Route('/portfolio_2', name: 'app_portfolio_2')]
+    public function portfolio_2(): Response
+    {
+        return $this->render('accueil/portfolio_2.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+
+    #[Route('/test', name: 'app_test')]
+    public function test(): Response
+    {
+        return $this->render('accueil/test.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+
+}
